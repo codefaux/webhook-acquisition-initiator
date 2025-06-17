7bd92abf703d2fe7426a901d7ac4e073bf301fe1
codefaux <codefaux@solacenet.org>
2025-06-17 15:53:19 -0700

    Add safe_move(...) to handle TEMP; fixes WAI_OUT_TEMP?

--- queue_manager.py
+++ queue_manager.py
@@ -1,10 +1,12 @@
 # queue_manager.py
 
+import errno
 import json
 import os
 import shutil
 import sys
 import threading
+import uuid
 
 import logger as _log
 from util import delete_item_file, load_item, save_item
@@ -206,6 +208,43 @@ def match_and_check(item: dict) -> dict | None:
     return item
 
 
+def safe_move(src, dst):
+    """Rename a file from ``src`` to ``dst``.
+
+    *   Moves must be atomic.  ``shutil.move()`` is not atomic.
+        Note that multiple threads may try to write to the cache at once,
+        so atomicity is required to ensure the serving on one thread doesn't
+        pick up a partially saved image from another thread.
+
+    *   Moves must work across filesystems.  Often temp directories and the
+        cache directories live on different filesystems.  ``os.rename()`` can
+        throw errors if run across filesystems.
+
+    So we try ``os.rename()``, but if we detect a cross-filesystem copy, we
+    switch to ``shutil.move()`` with some wrappers to make it atomic.
+    """
+    try:
+        os.rename(src, dst)
+    except OSError as err:
+
+        if err.errno == errno.EXDEV:
+            # Generate a unique ID, and copy `<src>` to the target directory
+            # with a temporary name `<dst>.<ID>.tmp`.  Because we're copying
+            # across a filesystem boundary, this initial copy may not be
+            # atomic.  We intersperse a random UUID so if different processes
+            # are copying into `<dst>`, they don't overlap in their tmp copies.
+            copy_id = uuid.uuid4()
+            tmp_dst = "%s.%s.tmp" % (dst, copy_id)
+            shutil.copyfile(src, tmp_dst)
+
+            # Then do an atomic rename onto the new name, and clean up the
+            # source image.
+            os.rename(tmp_dst, dst)
+            os.unlink(src)
+        else:
+            raise
+
+
 def download_item(item: dict) -> dict | None:
     from ytdlp_interface import download_video
 
@@ -233,10 +272,21 @@ def rename_and_move_item(item: dict) -> dict | None:
     tag_filepath = tag_filename(item.get("download_filename", ""))
     file_name = os.path.basename(tag_filepath)
 
-    if WAI_OUT_TEMP:  # NOT WORKING
-        shutil.copy(tag_filepath, os.path.abspath(WAI_OUT_PATH))
-        os.remove(tag_filepath)
+    if WAI_OUT_TEMP:  # NOT WORKING ?
+        safe_move(
+            tag_filepath,
+            os.path.join(os.path.abspath(WAI_OUT_PATH), file_name),
+        )
         _log.msg(f"Moved: {tag_filepath} \n\t-> To: {os.path.abspath(WAI_OUT_PATH)}")
+        safe_move(
+            tag_filepath.replace(".mkv", ".info.json"),
+            os.path.join(
+                os.path.abspath(WAI_OUT_PATH), file_name.replace(".mkv", ".info.json")
+            ),
+        )
+        _log.msg(
+            f"Moved: {tag_filepath.replace(".mkv", ".info.json")} \n\t-> To: {os.path.abspath(WAI_OUT_PATH)}"
+        )
 
     item["file_name"] = file_name
 
